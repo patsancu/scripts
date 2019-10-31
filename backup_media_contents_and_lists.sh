222057eb1a9d26a0f94415d97b982d3724a65496
patrick <patricksanzo@gmail.com>
2019-10-31 20:47:50 +0100

    refactor content listing backup logic into function

--- backup_media_contents_and_lists.sh
+++ backup_media_contents_and_lists.sh
@@ -23,32 +23,40 @@ else
     echo "$(date +'%Y%m%d-%H%M%S') A rhythmbox file was backed up" >> $BACKUP_LOG_FOLDER/logs.txt
 fi
 
+track_contents_of_folder_and_back_it_up(){
+    # First argument would be the folder
+    # Second argument should be the name of the file where the contents will be dumped
+    PATH_TO_FOLDER=$1
+    CONTENTS_FILE=$2
+    NAME_OF_TYPE_OF_DATA_TO_BE_SAVED=$3
+    echo "Tracking $NAME_OF_TYPE_OF_DATA_TO_BE_SAVED"
+    if [ -d "$PATH_TO_FOLDER" ]; then
+        find "$PATH_TO_FOLDER" -not -path '*/\.*' -print | sed -e 's;/*/;|;g;s;|; |;g' > $CONTENTS_FILE
+        git diff --exit-code $CONTENTS_FILE > /dev/null
+        if [ $? -eq 0 ]; then
+            echo "$(date +'%Y%m%d-%H%M%S') Nothing new in $NAME_OF_TYPE_OF_DATA_TO_BE_SAVED" >> $BACKUP_LOG_FOLDER/logs.txt
+        else
+            git add $CONTENTS_FILE;
+            git commit -m "$NAME_OF_TYPE_OF_DATA_TO_BE_SAVED contents $(date +'%Y%m%d-%H%M%S')"
+            echo "$(date +'%Y%m%d-%H%M%S') $NAME_OF_TYPE_OF_DATA_TO_BE_SAVED folder contents were backed up" >> $BACKUP_LOG_FOLDER/logs.txt
+        fi
+    fi
+}
 
 ##################
 ## Music contents
 ##################
+
 PATH_TO_MUSIC_FOLDER=/media/patrick/Toshiba/Música
 MUSIC_CONTENTS_FILE=music_contents.txt
-if [ -d $PATH_TO_MUSIC_FOLDER ]; then
-    find $PATH_TO_MUSIC_FOLDER -not -path '*/\.*' -print | sed -e 's;/*/;|;g;s;|; |;g' > $MUSIC_CONTENTS_FILE
-    git diff --exit-code $MUSIC_CONTENTS_FILE > /dev/null
-    if [ $? -eq 0 ]; then
-        echo "$(date +'%Y%m%d-%H%M%S') Nothing new in music" >> $BACKUP_LOG_FOLDER/logs.txt
-    else
-        git add $MUSIC_CONTENTS_FILE;
-        git commit -m "music contents $(date +'%Y%m%d-%H%M%S')"
-        echo "$(date +'%Y%m%d-%H%M%S') Music folder contents were backed up" >> $BACKUP_LOG_FOLDER/logs.txt
-    fi
-fi
-
-git push >> $BACKUP_LOG_FOLDER/logs.txt
-
+track_contents_of_folder_and_back_it_up "$PATH_TO_MUSIC_FOLDER" "$MUSIC_CONTENTS_FILE" "music"
 
 ##################
 ## IT Ebooks
 ##################
 IT_EBOOKS_FOLDER="/media/patrick/Toshiba/IT ebooks"
 IT_EBOOKS_CONTENT=it_ebooks_content.txt
+track_contents_of_folder_and_back_it_up "$IT_EBOOKS_FOLDER" "$IT_EBOOKS_CONTENT" "it ebooks"
 
 if [ -d "$IT_EBOOKS_FOLDER" ]; then
     find "$IT_EBOOKS_FOLDER" -not -path '*/\.*' -print | sed -e 's;/*/;|;g;s;|; |;g' > $IT_EBOOKS_CONTENT
