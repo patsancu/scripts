1da1fafd056f54bd0b6aa1ea970e3d54fbf228bb
Patrick Sanzo <patricksanzo@gmail.com>
2024-04-24 14:19:38 +0200

    2048: Fix source
    According to https://stackoverflow.com/questions/50386828/snprintf-function-not-declared

--- 2048.c
+++ 2048.c
@@ -21,6 +21,7 @@
 uint32_t score=0;
 uint8_t scheme=0;
 
+int snprintf(char *buf, size_t size, const char *fmt, ...);
 void getColor(uint8_t value, char *color, size_t length) {
 	uint8_t original[] = {8,255,1,255,2,255,3,255,4,255,5,255,6,255,7,255,9,0,10,0,11,0,12,0,13,0,14,0,255,0,255,0};
 	uint8_t blackwhite[] = {232,255,234,255,236,255,238,255,240,255,242,255,244,255,246,0,248,0,249,0,250,0,251,0,252,0,253,0,254,0,255,0};
